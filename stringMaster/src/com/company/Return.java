66b566175ed3c9f043f4c2b7d5d9e85a244fdba9
Max <mx.a.kovalenko@gmail.com>
2021-01-21 16:33:33 +0200

    Reforms
    First reform! 🥳🎉🎊

--- stringMaster/src/com/company/Return.java
+++ stringMaster/src/com/company/Return.java
@@ -6,7 +6,7 @@ public class Return {
 
     public static void returner(String a, String b){
         //int sum = a + b;
-        String sum = a + b;
+        String sum = a + " " + b;
         System.out.print(sum);
     }
 }
