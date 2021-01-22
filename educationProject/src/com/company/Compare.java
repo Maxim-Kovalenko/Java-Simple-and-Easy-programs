5d59c5ae3618d995a4fc8e124f1dc61971e2008b
Max <mx.a.kovalenko@gmail.com>
2021-01-22 18:51:54 +0200

    Refoms
    New refom in that project

--- educationProject/src/com/company/Compare.java
+++ educationProject/src/com/company/Compare.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 
 public class Compare {
     public static String compare(int[] array){
-        for (int i = 0, endI = array.length - 1; i < endI; i++){
-            boolean wasSwap = false;
-            for (int j = 0, endJ = endI - 1;j < endJ; j++){
-                if (array[j] > array[j+1]){
-                    int swap = array[j + 1];
-                    array[j] = array[j+1];
-                    array[j + 1] = swap;
-                    wasSwap = true;
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i] > array[i+1]){
+                    isSorted = false;
+
+                    buf = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = buf;
+                    String arrayString = Arrays.toString(array);
+                    System.out.println(arrayString);
                 }
-            }
-            if(!wasSwap){
-                break;
             }
         }
         String arrayString = Arrays.toString(array);
